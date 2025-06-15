b2b4bfd00e418eb318a6063e03bb9d533386af9e
Anjo Kolk <anjo.kolk@gmail.com>
2025-06-15 23:03:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ import streamlit_openai
 
 if "chat" not in st.session_state:
     st.session_state.chat = streamlit_openai.Chat()
+    st.session_state.chat._tools.append({
+                    "type": "mcp",
+                    "server_label": "KadasterServer",
+                    "server_url": "https://labs.test.kadaster.nl/mcp/server"
+                })
 
 st.session_state.chat.run()
