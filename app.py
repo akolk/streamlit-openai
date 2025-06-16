7deee5b8e3fb4e7d84c15988e44c2cd8f9ea633c
Anjo Kolk <anjo.kolk@gmail.com>
2025-06-16 23:16:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import streamlit_openai
 
 if "chat" not in st.session_state:
     st.session_state.chat = streamlit_openai.Chat(
-        instructions="Je bent kadaster assistent en je probeert altijd vragen met je bschikbare tools te beantwoorden in het Nederlands."
+        instructions="Je bent kadaster assistent en je probeert altijd vragen met je bschikbare tools te beantwoorden in het Nederlands.",
+        info_message="Deel geen persoonlijke informatie en AI kan het soms mis hebben.",
+        placeholder="Stel je vraag hier ....",
+        welcome_message="Hallo ik ben een AI assistent die kan helpen bij verschillende analyses en vragen over je leef omgeving."
     )
     st.session_state.chat._tools.append({
                     "type": "mcp",
