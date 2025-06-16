a5b0e001b4dfdfa2290742422edac46b8e77d8ac
Anjo Kolk <anjo.kolk@gmail.com>
2025-06-16 12:25:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ import streamlit as st
 import streamlit_openai
 
 if "chat" not in st.session_state:
-    kadasterserver = streamlit_openai.RemoteMCP(
-        server_label="kadasterserver",
-        server_url="https://labs.test.kadaster.nl/mcp/server",
-    )
+    st.session_state.chat = streamlit_openai.Chat()
+    st.session_state.chat._tools.append({
+                    "type": "mcp",
+                    "server_label": "KadasterServer",
+                    "server_url": "https://labs.test.kadaster.nl/mcp/server"
+                })
+    
+    #kadasterserver = streamlit_openai.RemoteMCP(
+    #    server_label="kadasterserver",
+    #    server_url="https://labs.test.kadaster.nl/mcp/server",
+    #)
 
-    st.session_state.chat = streamlit_openai.Chat(
-        mcps=[kadasterserver]
-    )
+    #st.session_state.chat = streamlit_openai.Chat(
+    #    mcps=[kadasterserver]
+    #)
     
 st.session_state.chat.run()
