5740a118a9ccd32ae464304b841292984bc39358
Anjo Kolk <anjo.kolk@gmail.com>
2025-06-19 13:59:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,37 +2,28 @@ import streamlit as st
 import streamlit_openai
 
 if "chat" not in st.session_state:
+
+    kadasterserver = streamlit_openai.RemoteMCP(
+        server_label="kadasterserver",
+        server_url="https://labs.test.kadaster.nl/mcp/server",
+    )
+
+    cbsserver = streamlit_openai.RemoteMCP(
+        server_label="cbsserver",
+        server_url="https://labs.test.kadaster.nl/mcp/cbs",
+    )
+    
+    thinkingserver = streamlit_openai.RemoteMCP(
+        server_label="thinkingserver",
+        server_url="https://labs.test.kadaster.nl/mcp/sequential-thinking",
+    )
+      
     st.session_state.chat = streamlit_openai.Chat(
         instructions="Je bent kadaster assistent en je probeert altijd vragen met je bschikbare tools te beantwoorden in het Nederlands.",
         info_message="Deel geen persoonlijke informatie en AI kan het soms mis hebben.",
         placeholder="Stel je vraag hier ....",
-        welcome_message="Hallo ik ben een AI assistent die kan helpen bij verschillende analyses en vragen over je leef omgeving."
+        welcome_message="Hallo ik ben een AI assistent die kan helpen bij verschillende analyses en vragen over je leef omgeving.",
+        mcps=[kadasterserver, cbsserver, thinkingserver]
     )
-    st.session_state.chat._tools.append({
-                    "type": "mcp",
-                    "server_label": "KadasterServer",
-                    "server_url": "https://labs.test.kadaster.nl/mcp/server",
-                    "require_approval": "never"
-                })
-    st.session_state.chat._tools.append({
-                    "type": "mcp",
-                    "server_label": "CBSServer",
-                    "server_url": "https://labs.test.kadaster.nl/mcp/cbs",
-                    "require_approval": "never"
-                })
-    st.session_state.chat._tools.append({
-                    "type": "mcp",
-                    "server_label": "ThinkingServer",
-                    "server_url": "https://labs.test.kadaster.nl/mcp/sequential-thinking",
-                    "require_approval": "never"
-                })
-    #kadasterserver = streamlit_openai.RemoteMCP(
-    #    server_label="kadasterserver",
-    #    server_url="https://labs.test.kadaster.nl/mcp/server",
-    #)
 
-    #st.session_state.chat = streamlit_openai.Chat(
-    #    mcps=[kadasterserver]
-    #)
-    
 st.session_state.chat.run()
