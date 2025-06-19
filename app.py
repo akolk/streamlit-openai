42a81eadb4161decdd8c73fa7f152fb5b705fa5f
Anjo Kolk <anjo.kolk@gmail.com>
2025-06-19 21:51:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,18 @@ if "chat" not in st.session_state:
         server_label="thinkingserver",
         server_url="https://labs.test.kadaster.nl/mcp/sequential-thinking",
     )
-      
+    
+    deepwiki = streamlit_openai.RemoteMCP(
+        server_label="deepwiki",
+        server_url="https://mcp.deepwiki.com/mcp",
+    )
+    
     st.session_state.chat = streamlit_openai.Chat(
         instructions="Je bent kadaster assistent en je probeert altijd vragen met je bschikbare tools te beantwoorden in het Nederlands.",
         info_message="Deel geen persoonlijke informatie en AI kan het soms mis hebben.",
         placeholder="Stel je vraag hier ....",
         welcome_message="Hallo ik ben een AI assistent die kan helpen bij verschillende analyses en vragen over je leef omgeving.",
-        mcps=[kadasterserver, cbsserver, thinkingserver],
+        mcps=[kadasterserver, cbsserver, deepwiki],
         allow_image_generation=True,
         model="o3",             # Select a reasoning model
         allow_web_search=False, # Disable web search
